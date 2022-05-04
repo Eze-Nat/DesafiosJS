d6b8893593ca62bd89a9569de48b3605ce12e362
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-05-04 12:10:22 -0300

    error

--- script.js
+++ script.js
@@ -1,19 +1,71 @@
-let fecha = Number(prompt(`INGRESE SU AÑO DE NACIMIENTO`));
+let opcion = mostrarMenu();
+let dineroIngresado = 0;
+let cantidadIngresada = 0;
 
-if (fecha <= 2004)  
-
-{
-    let nombre = prompt("Ingresa su nombre");
-    let apellido = prompt("Ingresa su apellido");
-
-    nombre = nombre.toUpperCase();
-    apellido = apellido.toUpperCase();
-
-    alert(`Bienvenido `+nombre+" "+apellido)
+if (opcion != 3) {
+    cantidadProducto(opcion);
+} else {
+    alert("GRACIAS");
 }
 
-else
-{
-    alert(`No sos mayor de edad`);
+
+function mostrarMenu() {
+    return prompt(`Seleccione un producto: 
+                     1. Vinos ($800)
+                     2. Quesos ($ 1200)
+                     3. Salir`);
+
+}
+
+function cantidadProducto(opcion) {
+    if (opcion == 1) {
+        pedirCantidad()
+    } else if (opcion == 2) {
+        pedirCantidad()
+    } else {
+        alert("opcion invalida")
+    }
+}
+
+
+function pedirCantidad(numMin) {
+    while (cantidadIngresada < numMin) {
+        let cantidad = Number(prompt("Ingrese cantidad"));
+
+        if (cantidad !== 0) {
+            alert("Cantidad invalida")
+        } else {
+            cantidadIngresada += cantidad;
+        }
+    }
+    function resultado(pedirCantidad)
+    {
+        return cantidad * 800;
+        return cantidad * 1200;    }
+    }
+    
+
+
+function venderProducto(opcion) {
+    if (opcion == 1) {
+        pedirDinero(resultado)
+    } else if (opcion == 2) {
+        pedirDinero(resultado)
+    } else {
+        alert("opcion invalida")
+    }
+}
+
+function pedirDinero(numMin) {
+    while (dineroIngresado < numMin) {
+        let dinero = Number(prompt("Ingrese un monto"));
+
+        if (dinero !== resultado) {
+            alert("monto invalido")
+        } else {
+            dineroIngresado += dinero;
+        }
+    }
+
 }
 
