64de3cb63e67f45cc974f189a9d55d8e79812b9d
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-05-04 09:08:41 -0300

    creando ciclo simple

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+let fecha = Number(prompt(`INGRESE SU AÑO DE NACIMIENTO`));
 
+if (fecha <= 2004)  
 
+{
+    let nombre = prompt("Ingresa su nombre");
+    let apellido = prompt("Ingresa su apellido");
 
+    nombre = nombre.toUpperCase();
+    apellido = apellido.toUpperCase();
+
+    alert(`Bienvenido `+nombre+" "+apellido)
+}
+
+else
+{
+    alert(`No sos mayor de edad`);
+}
 
