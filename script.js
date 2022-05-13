1fafaa4bd121f6000177782b17e882a7cfc357f5
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-05-12 23:13:23 -0300

    clase 6

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+let contador=0;
+let miArray= [];
 
 
+while(contador<=5){
+    let numero = Number(prompt("Ingrese Un Número"));
+    miArray.push(numero);
+    contador++;
+}
 
+let suma = 0;
+for(let i=0; i<miArray.length;i++)
+{
+    suma += miArray[i]; // es igual a  ====>>>> suma = suma + miArray[i];
+}
+
+console.log(suma);
+
+let multiplicacion=1;
+for(let i=0;i<miArray.length;i++)
+{
+    multiplicacion *= miArray[i];
+}
+
+console.log(multiplicacion);
 
