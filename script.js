1cdb8ab4e946954b7bb9390ea5a4b7947828dbf7
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-05-19 17:38:36 -0300

    array simple

--- script.js
+++ script.js
@@ -1,68 +1,19 @@
-class vino {
-    constructor(id, nombre, descripcion) {
-        this.id = id;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-    }
+class Stock {
+
+constructor(id,nombre,precio,)
+{
+    this.id=id;
+    this.nombre=nombre;
+    this.precio=precio;
 }
-
-const vinos = [];
-
-mostrarMenu();
-
-
-function mostrarMenu() {
-    let opcion = 0;
-
-    while (opcion !== 5) {
-        opcion = Number(prompt(`Seleccione una acción:
-                           1. Agregar vino
-                           2. Eliminar vino
-                           3. Modificar vino
-                           4. Listar vinos
-                           5. Salir`));
-
-        switch (opcion) {
-            case 1: {
-                agregarVino();
-                break;
-            }
-            default: {
-                alert("opcion inválida");
-            }
-
-        }
-
-    }
 }
+const vino1 = new Stock(1, "Galileo Blend Tinto", 950);
+const vino2 = new Stock(2, "Pragmatico Malbec", 820);
+const vino3 = new Stock(3, "Pragmatico Rosado", 650);
+const vino4 = new Stock(4, "Ejes Malbec", 500);
+const vino5 = new Stock(5, "Ejes Cabernet", 500);
 
-function agregarVino() {
-    let id = 1;
-    if (vinos.length > 0) {
-        id = vinos[vinos.length - 1].id + 1;
-    }
 
-    let nombre = prompt("ingrese un nombre");
-    let descripcion = prompt("ingrese una descripcion");
-    let vino = new vino(id, nombre, descripcion);
+const vinos = [vino1, vino2, vino3, vino4, vino5];
 
-    vinos.push(vino);
-}
-
-console.log(vinos)
-
-let stock = 0;
-let miArray = [];
-while (stock < 999) {
-    let numero = Number(prompt("Ingrese un numero"));
-    miArray.push(numero);
-    contador++;
-}
-console.log(miArray);
-
-let sumaStock = 0;
-for(let i=0;i<miArray.length; i++){
-    suma += miArray[i]
-}
-
-console.log("STOCK", suma)
+console.log(vinos);
