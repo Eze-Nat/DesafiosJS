5d7842c030db01c4ee50b9ca4515f71dd376cb34
Eze-Nat <ezequiel_natale@yahoo.com.ar>
2022-05-19 14:29:42 -0300

    buscar errores

--- script.js
+++ script.js
@@ -1,23 +1,68 @@
-const vino = [
-    {
-        id:1,
-        nombre:"Mimi malbec merlot",
-        precio:860,
-    },
-    {
-        id: 2,
-        nombre:"Casa Araujo cask malbec",
-        precio: 850,
-    },
-    {
-        id: 3,
-        nombre:"Pragmatico Malbec",
-        precio: 820,
-    },
-
-]
-
-for (const vinos of vino) {
-    console.log(vino.id);
-    console.log(vino.nombre);
+class vino {
+    constructor(id, nombre, descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
 }
+
+const vinos = [];
+
+mostrarMenu();
+
+
+function mostrarMenu() {
+    let opcion = 0;
+
+    while (opcion !== 5) {
+        opcion = Number(prompt(`Seleccione una acción:
+                           1. Agregar vino
+                           2. Eliminar vino
+                           3. Modificar vino
+                           4. Listar vinos
+                           5. Salir`));
+
+        switch (opcion) {
+            case 1: {
+                agregarVino();
+                break;
+            }
+            default: {
+                alert("opcion inválida");
+            }
+
+        }
+
+    }
+}
+
+function agregarVino() {
+    let id = 1;
+    if (vinos.length > 0) {
+        id = vinos[vinos.length - 1].id + 1;
+    }
+
+    let nombre = prompt("ingrese un nombre");
+    let descripcion = prompt("ingrese una descripcion");
+    let vino = new vino(id, nombre, descripcion);
+
+    vinos.push(vino);
+}
+
+console.log(vinos)
+
+let stock = 0;
+let miArray = [];
+while (stock < 999) {
+    let numero = Number(prompt("Ingrese un numero"));
+    miArray.push(numero);
+    contador++;
+}
+console.log(miArray);
+
+let sumaStock = 0;
+for(let i=0;i<miArray.length; i++){
+    suma += miArray[i]
+}
+
+console.log("STOCK", suma)
